f95b91ed04d2de9a9b11183f7c96918732f632a1
irshathahamed21 <21irshathkhan@gmail.com>
2022-12-03 12:42:38 +0530

    file completed till variable names

--- python_basics/python.py
+++ python_basics/python.py
@@ -6,4 +6,43 @@ print("hello world")
 
 # creating variables
 x = 5
+y= "john"
+print(x,y)
+
+# If you want to specify the data type of a variable, this can be done with casting.
+z = str(x)
+z1 = int(x)
+z2 = float(x)
+print(z, z1,z2)
+
+#You can get the data type of a variable with the type() function.
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+#String variables can be declared either by using single or double quotes:
+
+a = "Irshath"
+b = 'Irshath'
+print(a,b)
+
+# Variable names are case-sensitive.
+marks = 100
+MARKS = 500
+
+print(marks, MARKS)
+
+# Variable Names
+"""
+A variable can have a short name (like x and y) or a more descriptive name (age, carname, total_volume). 
+Rules for Python variables:
+A variable name must start with a letter or the underscore character
+A variable name cannot start with a number
+A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+Variable names are case-sensitive (age, Age and AGE are three different variables)
+"""
+
+
+
 
