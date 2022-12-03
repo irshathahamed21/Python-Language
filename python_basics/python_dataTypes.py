39fc3b6551496ec4baadc20a644fe6737f29509e
irshathahamed21 <21irshathkhan@gmail.com>
2022-12-03 13:04:36 +0530

    python data types

---
+++ python_basics/python_dataTypes.py
@@ -0,0 +1,17 @@
+type_1_str = "helloword"
+type_2_int = 2
+type_3_float = 2.3
+type_4_list = ["hello", 2, 2.3]
+type_5_complex = 22.5j
+type_6_tuple = ("pomegranate", "pineapple", "watermelon")
+type_7_range = range(4)
+type_8_dict = {"name":"irshath", "hobbies":"coding"}
+type_9_set = {"set" ,2 ,3}
+type_10_bool = True
+type_11_noneType = None
+type_12_bytes = b"hello"
+type_13_bytearray = bytearray(2)
+type_14_memoryview = memoryview(type_12_bytes)
+
+
+print(type_1_str, type_2_int, type_3_float, type_4_list, type_5_complex, type_6_tuple, type_7_range, type_8_dict, type_9_set, type_10_bool, type_11_noneType, type_12_bytes, type_13_bytearray, type_14_memoryview)
