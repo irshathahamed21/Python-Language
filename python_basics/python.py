be958203cc12433d60bf22d62ae970d25a084978
irshathahamed21 <21irshathkhan@gmail.com>
2022-12-03 12:31:24 +0530

    started coding python

---
+++ python_basics/python.py
@@ -0,0 +1,9 @@
+#python comments anything starts with # at start then python will ignore 
+# showing space will python error
+
+# for printing 
+print("hello world")
+
+# creating variables
+x = 5
+
